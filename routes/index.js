66751694b94326d577a2dc6c56583cd4f351baa5
Parker McCrary <parkertomm3@gmail.com>
2020-11-30 16:51:25 -0500

    Test 'Book' model & communication with DB

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
+// Import 'Book' model
+const { Book } = require('../models/');
+
+// Handler function to wrap each route
+function asyncHandler(cb) {
+  return async(req, res, next) => {
+    try {
+      await cb(req, res, next);
+    } catch(err) {
+      res.status(500).send(err);
+    }
+  }
+}
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', asyncHandler(async (req, res) => {
+  res.redirect('/books');
+}));
+
+// Get Books
+router.get('/books', asyncHandler(async (req, res) => {
+  const books = await Book.findAll();
+  res.json(books)
+}))
 
 module.exports = router;
